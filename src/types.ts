2a1c168a840393a8b7d06dd75abfca65b5d17af8
Spencer Kinsey-Korzym <34325816+huhu72@users.noreply.github.com>
2023-01-27 21:17:55 -0500

    Added initial logic to get player data

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface BannerTypes {
+  summoner: string;
+}
+export interface PlayerTypes {
+  summonerName: string;
+}
+export interface Summoner {
+  accountId?: string;
+  profileIconId?: number;
+  summonerName?: string;
+  encriptedId?: string;
+  puuid?: string;
+  summonerLevel?: number;
+  tier?: string;
+  rank?: string;
+  leaguePoints?: number;
+  wins?: number;
+  losses?: number;
+}
